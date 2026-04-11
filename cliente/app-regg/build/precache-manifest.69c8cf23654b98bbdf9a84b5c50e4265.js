e05cdab5bd0904862997590f309d2b7ee2114a60
Rodrigo Ezequiel Gimenez Giaimo <gimenezgiaimo.rodrigo@gmail.com>
2026-04-11 17:08:06 -0300

    Changes to be committed: 	modified:   cliente/app-regg/build/asset-manifest.json 	modified:   cliente/app-regg/build/index.html 	deleted:    cliente/app-regg/build/precache-manifest.3ab645baba8555cb8db6256dfda78478.js 	new file:   cliente/app-regg/build/precache-manifest.69c8cf23654b98bbdf9a84b5c50e4265.js 	modified:   cliente/app-regg/build/service-worker.js 	new file:   cliente/app-regg/build/static/css/2.032362bf.chunk.css 	new file:   cliente/app-regg/build/static/css/2.032362bf.chunk.css.map 	new file:   cliente/app-regg/build/static/css/main.c5870ebf.chunk.css 	new file:   cliente/app-regg/build/static/css/main.c5870ebf.chunk.css.map 	renamed:    cliente/app-regg/build/static/js/2.01a63e67.chunk.js -> cliente/app-regg/build/static/js/2.18bc1283.chunk.js 	renamed:    cliente/app-regg/build/static/js/2.01a63e67.chunk.js.LICENSE.txt -> cliente/app-regg/build/static/js/2.18bc1283.chunk.js.LICENSE.txt 	renamed:    cliente/app-regg/build/static/js/2.01a63e67.chunk.js.map -> cliente/app-regg/build/static/js/2.18bc1283.chunk.js.map 	deleted:    cliente/app-regg/build/static/js/main.1e51d822.chunk.js 	deleted:    cliente/app-regg/build/static/js/main.1e51d822.chunk.js.map 	new file:   cliente/app-regg/build/static/js/main.95a3e0df.chunk.js 	new file:   cliente/app-regg/build/static/js/main.95a3e0df.chunk.js.map 	new file:   cliente/app-regg/package-lock.json 	modified:   cliente/app-regg/package.json 	modified:   server/package.json

---
+++ cliente/app-regg/build/precache-manifest.69c8cf23654b98bbdf9a84b5c50e4265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41604724dc2af7af727f2d353f55ba90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52ab484ca14fd5171117",
+    "url": "/static/css/2.032362bf.chunk.css"
+  },
+  {
+    "revision": "1b906b8aa0655b79519a",
+    "url": "/static/css/main.c5870ebf.chunk.css"
+  },
+  {
+    "revision": "52ab484ca14fd5171117",
+    "url": "/static/js/2.18bc1283.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.18bc1283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b906b8aa0655b79519a",
+    "url": "/static/js/main.95a3e0df.chunk.js"
+  },
+  {
+    "revision": "31492c160b7b38050b72",
+    "url": "/static/js/runtime-main.c1f08149.js"
+  }
+]);
--- cliente/app-regg/build/service-worker.js
+++ cliente/app-regg/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ab645baba8555cb8db6256dfda78478.js"
+  "/precache-manifest.69c8cf23654b98bbdf9a84b5c50e4265.js"
 );
 
 self.addEventListener('message', (event) => {
